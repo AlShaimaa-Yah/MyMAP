e3ebb5c7c86519ea44b69e7e25993edd0f7359be
AlShaimaa-Yah <35180096+AlShaimaa-Yah@users.noreply.github.com>
2018-02-11 21:19:43 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+#Neighborhood Map Project
+In this project, the task is to develop a single page application featuring a map of a neighborhood.
+This app is to demonstrate the ability to use a js framework (`knockout.js`) & Google Maps API.
+
+##Features
+The site is fully responsive and will run on your tiniest screens all the way to the largest screen you can think of.
+
+##API
+Google Maps API is used here to show the map and generate the markers etc.
+
+##Here is The code
+You can find a live running site of this code [here]()
+
+##All Resources Used in this Project
+
+
+Google Maps API -
+https://developers.google.com/maps/documentation/javascript/tutorial
+And https://www.youtube.com/watch?v=Zxf1mnP5zcw
+
+color
+https://www.w3schools.com/colors/colors_picker.asp
+
+I get all places from Here:
+https://gps-coordinates.org/mecca-latitude.php
+
+Multiple Markers, Multiple Infowindows
+http://you.arenot.me/2010/06/29/google-maps-api-v3-0-multiple-markers-multiple-infowindows/
+
+Some KnockoutJS information help me
+http://knockoutjs.com/documentation/textinput-binding.html
---
+++ js/app.js
@@ -0,0 +1,165 @@
+//Here I put all location
+  var MyLocation = {
+    currentPlace: ko.observable(null),
+    markers: [
+    {
+        title: 'Kabah',
+        lat: 21.422487,
+        lng:  39.826206,
+        highlight: ko.observable(false)
+    },
+    {
+        title: 'AlNor Hospital',
+        lat: 21.385031451289365,
+        lng: 39.860544204711914,
+        highlight: ko.observable(false)
+    },
+    {
+        title: 'Ad Diyafah',
+        lat: 21.43584459802708,
+        lng: 39.8001709068194,
+        highlight: ko.observable(false)
+    },
+    {
+        title: 'Albaik',
+        lat: 21.421111484957944,
+        lng: 39.82166290283203,
+        highlight: ko.observable(false)
+    },
+    {
+        title: 'Ghar Thowr',
+        lat: 21.37723891309099,
+        lng: 39.84972953796387,
+        highlight: ko.observable(false)
+    },
+    {
+        title: 'Ghar Hira',
+        lat: 21.4575864,
+        lng: 39.859275600000046,
+        highlight: ko.observable(false)
+    },
+    {
+        title: 'Jarir',
+        lat: 21.44484033655246,
+        lng: 39.86045837402344,
+        highlight: ko.observable(false)
+    },
+    {
+        title: 'Abraj Albait',
+        lat: 21.4187514,
+        lng: 39.82555639999998,
+        highlight: ko.observable(false)
+    }
+]
+};
+
+
+var ViewModel = function() {
+  var self = this;
+  var map, geocoder, bounds, mywindow;
+
+  self.markerArray = ko.observableArray();
+  self.query = ko.observable('');
+
+  //initialization my map by using map markers from MyLocation data.
+  var initMap = function() {
+    // Check if Google Maps object exists. If it does, create map. If not, display error
+    if(typeof window.google === 'object' && typeof window.google.maps === 'object') {
+      var mapOptions = {
+        disableDefaultUI: true
+      };
+      //Here it will start to creat map of my places
+      map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+      geocoder = new google.maps.Geocoder();
+      bounds = new google.maps.LatLngBounds();
+      mywindow = new google.maps.InfoWindow({
+        content: null
+      });
+      //Here it will start creat a marker of my places
+      var markerList = MyLocation.markers;
+      for(var x = 0; x < markerList.length; x++) {
+        var markPostion = new google.maps.LatLng(
+          markerList[x].lat,
+          markerList[x].lng
+        );
+
+        var marker = new google.maps.Marker({
+          position: markPostion,
+          map: map,
+          title: markerList[x].title,
+          highlight: markerList[x].highlight
+        });
+
+        //Add click event listener to create infowindow for each marker object
+        //Use Google geocoder to pull physical address from lat/lng position data
+        google.maps.event.addListener(marker, 'click', function() {
+          var that = this;
+          geocoder.geocode({'latLng': that.position}, function(results, status) {
+            if(status == google.maps.GeocoderStatus.OK) {
+              if(results[0]){
+                var address = results[0].formatted_address;
+                mywindow.setContent("<span class='title'>" + that.title);
+              }
+            }
+          });
+
+          mywindow.open(map, that);
+          clearMarkers();
+
+          //Modify marker show list to select
+          that.highlight(true);
+
+          //Move map viewport to the selected item
+          map.panTo(that.position);
+          MyLocation.currentMarker(that);
+        });
+
+        // Modify map viewport to include new map marker
+        bounds.extend(markPostion);
+
+        //Add marker to array
+        self.markerArray.push(marker);
+      }
+      //Resize map to fit all markers
+      map.fitBounds(bounds);
+      map.setCenter(bounds.getCenter());
+
+      //Check window size
+      checkWindowSize();
+    } else {
+      //if no google object found, display error
+      self.mapUnavailable(true);
+    }
+  }();
+//******************************************************************************************
+
+    /*Knockout computed observable will filter and return items that match,
+    And list of locations will be show.*/
+  self.filteredArray = ko.computed(function() {
+    return ko.utils.arrayFilter(self.markerArray(), function(marker) {
+      return marker.title.toLowerCase().indexOf(self.query().toLowerCase()) !== -1;
+    });
+  }, self);
+
+
+  //Highlight map marker if list item is clicked.
+  self.selectItem = function(listItem) {
+    google.maps.event.trigger(listItem, 'click');
+  };
+
+  //Toggle showing marker list
+  self.toggleList = function() {
+    self.showList(!self.showList());
+  };
+
+
+  // Helper function to check viewport width, called only on initialization of map.
+  function checkWindowSize() {
+    if($(window).width() < 400){
+      self.showList(false);
+    }
+  }
+};
+
+
+ko.applyBindings(new ViewModel());
